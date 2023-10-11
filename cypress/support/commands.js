a9661dbfdeeed38d005cdf1b862c705a40f91f3a
Hosana <hosanabarceloscandido@gmail.com>
2023-10-11 11:42:23 -0300

    feat: cria comando de login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,12 @@
 Cypress.Commands.add('getByData', (seletor) => {
-  return cy.get(`[data-test=${seletor}]`)
+  return cy.get(`[data-test=${seletor}]`);
 })
+
+Cypress.Commands.add('login', (email, password) => {
+    cy.visit('/');
+    cy.getByData('botao-login').click();
+    cy.getByData('email-input').type(email);
+    cy.getByData('senha-input').type(password);
+    cy.getByData('botao-enviar').click();
+    cy.url().should('contain', '/home');
+});
