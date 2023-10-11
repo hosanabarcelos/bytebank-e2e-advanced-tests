d83f7d5bbd8a19b0aaabc0f18d061651a650d055
Hosana <hosanabarceloscandido@gmail.com>
2023-10-11 16:52:43 -0300

    test: add teste com condicional de dispositivo mobile

--- cypress/e2e/paginas.cy.js
+++ cypress/e2e/paginas.cy.js
@@ -1,16 +1,22 @@
-describe('Testing multiple pages', () => {
-  it('you should be able to access the cards page', {browser: 'edge'}, ()=>{
-    cy.visit('/')
-    cy.getByData('botao-login').click()
-    cy.getByData('email-input').type('neilton@alura.com')
-    cy.getByData('senha-input').type('123456')
-    cy.getByData('botao-enviar').click()
+import { isMobile } from '../support/utils';
 
+describe('Testing multiple pages', () => {
+  it('you should be able to access the cards page', ()=>{
+    cy.login(Cypress.env('email'), Cypress.env('password'));
+
+    cy.visit('/home');
     cy.location('pathname').should('eq','/home')
 
-    cy.getByData('app-home').find('a').eq(1).click()
+    if (isMobile()) {
+        cy.getByData('menu-burguer').should('be.visible');
+        cy.getByData('menu-burguer').click();
+        cy.getByData('menu-lateral').find('a').eq(2).click();
+      } else {
+        cy.getByData('app-home').find('a').eq(2).click();
+    }
+
     cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões')
 
     cy.location('pathname').should('eq', '/home/cartoes')
-  })
- })
+  });
+});
