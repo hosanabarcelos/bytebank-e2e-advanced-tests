904edb675390f86399a077b0d826e9dfe8927abd
Hosana <hosanabarceloscandido@gmail.com>
2023-10-11 11:42:48 -0300

    feat: cria caso de teste usando comando de login

--- cypress/e2e/formularioLogin.cy.js
+++ cypress/e2e/formularioLogin.cy.js
@@ -1,20 +1,24 @@
 describe('Login Form', ()=>{
   beforeEach(()=>{
-    cy.visit('http://localhost:3000')
-  })
+    cy.login('alien@email.com', '123456')
+  });
+
+  it.only('access the home page', ()=>{
+    cy.getByData('titulo-boas-vindas').should('contain', 'Bem vindo de volta!');
+  });
 
   it('should not allow an invalid email', ()=>{
     cy.getByData('botao-login').click()
-    cy.getByData('email-input').type('neilton@alura')
+    cy.getByData('email-input').type('alienigena@email.com')
     cy.getByData('senha-input').type('123456')
     cy.getByData('botao-enviar').click()
     cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido')
-  })
+  });
 
-  it.only('should not allow a blank field', ()=>{
+  it('should not allow a blank field', ()=>{
     cy.getByData('botao-login').click()
     cy.getByData('senha-input').type('123456')
     cy.getByData('botao-enviar').click()
     cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo email é obrigatório')
-  })
-})
+  });
+});
