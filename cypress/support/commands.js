5a2f8fc213b71e028d31faf1a8bb51256d5bd33e
Hosana <hosanabarceloscandido@gmail.com>
2023-10-11 11:49:45 -0300

    feat: add comando de login em sessão

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,10 +3,12 @@ Cypress.Commands.add('getByData', (seletor) => {
 })
 
 Cypress.Commands.add('login', (email, password) => {
-    cy.visit('/');
-    cy.getByData('botao-login').click();
-    cy.getByData('email-input').type(email);
-    cy.getByData('senha-input').type(password);
-    cy.getByData('botao-enviar').click();
-    cy.url().should('contain', '/home');
+    cy.session([email, password], () => {
+        cy.visit('/');
+        cy.getByData('botao-login').click();
+        cy.getByData('email-input').type(email);
+        cy.getByData('senha-input').type(password);
+        cy.getByData('botao-enviar').click();
+        cy.url().should('contain', '/home');
+    });
 });
